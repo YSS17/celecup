fe8fb54b2d5a9afaaefa5f7d76ad4f18f996e210
Yan Santos <yan17souza@hotmail.com>
2020-06-04 15:40:08 -0300

    show page implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_172907) do
+ActiveRecord::Schema.define(version: 2020_06_04_172602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,11 +49,12 @@ ActiveRecord::Schema.define(version: 2020_06_03_172907) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "category"
-    t.integer "price"
+    t.float "price"
     t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
