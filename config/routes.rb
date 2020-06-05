3b0e2ecaca4e3fb4dd9d629d98fd0c4e5fe21459
rafael osawa <osawa.rafael@gmail.com>
2020-06-05 20:26:42 +0300

    profile-page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :users, only: [:show]
   resources :products do
     resources :carts, only: [:create]
   end
+  get "/show", to: "pages#show"
+  get "/orders", to: "pages#orders"
 
 end
